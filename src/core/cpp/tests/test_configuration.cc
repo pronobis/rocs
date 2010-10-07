491191906bc526828e0a1a8fe31c293456a0fce6
Andrzej Pronobis <a.pronobis@gmail.com>
2010-10-08 01:19:21 +0200

    Improved the tests for configuration.

--- src/core/cpp/tests/test_configuration.cc
+++ src/core/cpp/tests/test_configuration.cc
@@ -32,6 +32,7 @@
 #include <boost/test/unit_test.hpp>
 // ROCS
 #include <rocs/core/Configuration.h>
+#include <rocs/core/debug.h>
 
 using namespace rocs::core;
 using namespace std;
@@ -48,12 +49,14 @@ BOOST_AUTO_TEST_CASE( command_line_args_single )
 
 	// Let configuration parse the arguments
 	Configuration config(argc, argv);
-	bool wasFound;
-	int value1 = config.getValue("one.two.three", 2, wasFound);
-	int value2 = config.getValue("one.two.three", 2);
 
 	// Print the configuration
 	config.printConfiguration();
+
+	// Get values
+	bool wasFound;
+	int value1 = config.getValue("one.two.three", 2, wasFound);
+	int value2 = config.getValue("one.two.three", 2);
 
 	// Check the output
 	BOOST_REQUIRE( wasFound );
@@ -74,11 +77,13 @@ BOOST_AUTO_TEST_CASE( command_line_args_list1 )
 
 	// Let configuration parse the arguments
 	Configuration config(argc, argv);
-	vector<int> values;
-	config.getValueList("list", "key", values);
 
 	// Print the configuration
 	config.printConfiguration();
+
+	// Get values
+	vector<int> values;
+	config.getValueList("list", "key", values);
 
 	// Check the output
 	BOOST_REQUIRE( values.size() == 2 );
@@ -87,7 +92,7 @@ BOOST_AUTO_TEST_CASE( command_line_args_list1 )
 }
 
 /*!
- * Case 2
+ * Case 3
  * Testing command line argument parsing for lists.
  */
 BOOST_AUTO_TEST_CASE( command_line_args_list2 )
@@ -99,11 +104,13 @@ BOOST_AUTO_TEST_CASE( command_line_args_list2 )
 
 	// Let configuration parse the arguments
 	Configuration config(argc, argv);
-	vector<int> values;
-	config.getValueList("list", "key", values);
 
 	// Print the configuration
 	config.printConfiguration();
+
+	// Get values
+	vector<int> values;
+	config.getValueList("list", "key", values);
 
 	// Check the output
 	BOOST_REQUIRE( values.size() == 3 );
@@ -112,3 +119,173 @@ BOOST_AUTO_TEST_CASE( command_line_args_list2 )
 	BOOST_REQUIRE( values[2] == 4 );
 }
 
+/*!
+ * Case 4
+ * Testing INI file parsing.
+ */
+BOOST_AUTO_TEST_CASE( ini_simple )
+{
+	Configuration config;
+	config.addConfigFile(ROCS_DIR "/config/test/test.ini");
+
+	// Print the configuration
+	config.printConfiguration();
+
+	bool wasFound1;
+	int value1 = config.getValue("one4.two4_1", 100, wasFound1);
+	bool wasFound2;
+	string value2 = config.getValue("one4.two4_2", string(""), wasFound2);
+	bool wasFound3;
+	float value3 = config.getValue("one5.two5", 100.0, wasFound3);
+
+	// Check the output
+	BOOST_REQUIRE( wasFound1 );
+	BOOST_REQUIRE( value1 == 1 );
+	BOOST_REQUIRE( wasFound2 );
+	BOOST_REQUIRE( value2 == string("This is text") );
+	BOOST_REQUIRE( wasFound3 );
+	BOOST_REQUIRE( (value3 > 2.29) && (value3 < 2.31) );
+}
+
+
+/*!
+ * Case 5
+ * Testing JSON file parsing.
+ */
+BOOST_AUTO_TEST_CASE( json_simple )
+{
+	Configuration config;
+	config.addConfigFile(ROCS_DIR "/config/test/test.json");
+
+	// Print the configuration
+	config.printConfiguration();
+
+	// Get values
+	// Single
+	bool wasFound1;
+	int value1 = config.getValue("one1", 100, wasFound1);
+	bool wasFound2;
+	float value2 = config.getValue("one2", 100.0, wasFound2);
+	bool wasFound3;
+	string value3 = config.getValue("one3", string(""), wasFound3);
+	bool wasFound4;
+	int value4 = config.getValue("one4.two4_1", 100, wasFound4);
+	bool wasFound5;
+	string value5 = config.getValue<string>("one4.two4_2", "", wasFound5);
+	bool wasFound6;
+	float value6 = config.getValue("one5.two5", 100.0, wasFound6);
+	bool wasFound7;
+	int value7 = config.getValue("one6.two6.three6", 100, wasFound7);
+	// Lists
+	vector<int> values1;
+	config.getValueList("list1", "key1", values1);
+	vector<int> values2;
+	config.getValueList("list2", "key2", values2);
+	// Default
+	bool wasFound8;
+	int value8 = config.getValue("one10.two10", 100, wasFound8);
+
+	// Check the output
+	BOOST_REQUIRE( wasFound1 );
+	BOOST_REQUIRE_EQUAL( value1, 1 );
+	BOOST_REQUIRE( wasFound2 );
+	BOOST_REQUIRE_GT( value2, 2.29 );
+	BOOST_REQUIRE_LT( value2, 2.31 );
+	BOOST_REQUIRE( wasFound3 );
+	BOOST_REQUIRE_EQUAL( value3, string("This is text") );
+	BOOST_REQUIRE( wasFound4 );
+	BOOST_REQUIRE_EQUAL( value4, 1 );
+	BOOST_REQUIRE( wasFound5 );
+	BOOST_REQUIRE_EQUAL( value5, string("This is text") );
+	BOOST_REQUIRE( wasFound6 );
+	BOOST_REQUIRE_GT( value6, 2.29 );
+	BOOST_REQUIRE_LT( value6, 2.31 );
+	BOOST_REQUIRE( wasFound7 );
+	BOOST_REQUIRE_EQUAL( value7, 6 );
+	BOOST_REQUIRE_EQUAL( values1.size(), 3 );
+	BOOST_REQUIRE_EQUAL( values1[0], 1 );
+	BOOST_REQUIRE_EQUAL( values1[1], 2 );
+	BOOST_REQUIRE_EQUAL( values1[2], 4 );
+	BOOST_REQUIRE_EQUAL( values2.size(), 3 );
+	BOOST_REQUIRE_EQUAL( values2[0], 1 );
+	BOOST_REQUIRE_EQUAL( values2[1], 2 );
+	BOOST_REQUIRE_EQUAL( values2[2], 4 );
+	BOOST_REQUIRE( !wasFound8 );
+	BOOST_REQUIRE_EQUAL( value8, 100 );
+}
+
+
+/*!
+ * Case 6
+ * Testing XML file parsing.
+ */
+BOOST_AUTO_TEST_CASE( xml_simple )
+{
+	Configuration config;
+	config.addConfigFile(ROCS_DIR "/config/test/test.xml");
+
+	// Print the configuration
+	config.printConfiguration();
+
+	// Get values
+	// Single
+	bool wasFound1;
+	int value1 = config.getValue("one1", 100, wasFound1);
+	bool wasFound2;
+	float value2 = config.getValue("one2", 100.0, wasFound2);
+	bool wasFound3;
+	string value3 = config.getValue("one3", string(""), wasFound3);
+	bool wasFound4;
+	int value4 = config.getValue("one4.two4_1", 100, wasFound4);
+	bool wasFound5;
+	string value5 = config.getValue<string>("one4.two4_2", "", wasFound5);
+	bool wasFound6;
+	float value6 = config.getValue("one5.two5", 100.0, wasFound6);
+	bool wasFound7;
+	int value7 = config.getValue("one6.two6.three6", 100, wasFound7);
+	// Lists
+	vector<int> values1;
+	config.getValueList("list1", "key1", values1);
+	vector<int> values2;
+	config.getValueList("list2", "key2", values2);
+	// Default
+	bool wasFound8;
+	int value8 = config.getValue("one10.two10", 100, wasFound8);
+
+
+	// Check the output
+	BOOST_REQUIRE( wasFound1 );
+	BOOST_REQUIRE_EQUAL( value1, 1 );
+	BOOST_REQUIRE( wasFound2 );
+	BOOST_REQUIRE_GT( value2, 2.29 );
+	BOOST_REQUIRE_LT( value2, 2.31 );
+	BOOST_REQUIRE( wasFound3 );
+	BOOST_REQUIRE_EQUAL( value3, string("This is text") );
+	BOOST_REQUIRE( wasFound4 );
+	BOOST_REQUIRE_EQUAL( value4, 1 );
+	BOOST_REQUIRE( wasFound5 );
+	BOOST_REQUIRE_EQUAL( value5, string("This is text") );
+	BOOST_REQUIRE( wasFound6 );
+	BOOST_REQUIRE_GT( value6, 2.29 );
+	BOOST_REQUIRE_LT( value6, 2.31 );
+	BOOST_REQUIRE( wasFound7 );
+	BOOST_REQUIRE_EQUAL( value7, 6 );
+	BOOST_REQUIRE_EQUAL( values1.size(), 3 );
+	BOOST_REQUIRE_EQUAL( values1[0], 1 );
+	BOOST_REQUIRE_EQUAL( values1[1], 2 );
+	BOOST_REQUIRE_EQUAL( values1[2], 4 );
+	BOOST_REQUIRE_EQUAL( values2.size(), 3 );
+	BOOST_REQUIRE_EQUAL( values2[0], 1 );
+	BOOST_REQUIRE_EQUAL( values2[1], 2 );
+	BOOST_REQUIRE_EQUAL( values2[2], 4 );
+	BOOST_REQUIRE( !wasFound8 );
+	BOOST_REQUIRE_EQUAL( value8, 100 );
+}
+
+
+// TODO: add char* getValue
+// TODO: add parsed config arguments
+// TODO: add includes in the command line and in the other config file formats
+// TODO: add tests for includes
+// TODO: add error handling in the config file reader
+// TODO: remove printChildren if not explicitly run!!
