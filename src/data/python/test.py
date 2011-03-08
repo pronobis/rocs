47de803d5beadecd853cc0661ec54a5576b98284
André Susano Pinto <andresusanopinto@gmail.com>
2011-03-08 16:26:22 +0100

    Added code to show how to extract probs from the databases.

--- src/data/python/test.py
+++ src/data/python/test.py
@@ -21,5 +21,19 @@ def testVPC():
       if seq.HasCategoryEdge(cat1, cat2):
         print "%s->%s => connection" % (cat1, cat2)
 
-sequences = list(COLD.LoadSequences()) + list(VPC.LoadSequences())
-print sequences
+sequences = list(COLD.LoadSequences()) # + list(VPC.LoadSequences())
+room_cats = set(cat for seq in sequences for cat in seq.category.keys())
+
+def SequencesWithConnection(a, b):
+  return len([seq for seq in sequences if seq.HasCategoryEdge(a, b)])
+    
+def SequencesWithRooms(a, b):
+  return len([seq for seq in sequences if (a and b) in seq.category])
+
+p_connection = dict()
+for a in room_cats:
+  for b in room_cats:
+    if a < b:
+      p_connection[a, b] = SequencesWithConnection(a, b)
+
+print p_connection
