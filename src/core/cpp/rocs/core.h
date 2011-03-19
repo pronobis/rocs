220940fa4bce5821f32e7f9a3762ddb9d81f4a5b
Andrzej Pronobis <a.pronobis@gmail.com>
2011-03-19 19:46:45 +0100

    Added main module include files.

---
+++ src/core/cpp/rocs/core.h
@@ -0,0 +1,34 @@
+// ==================================================================
+// ROCS - Toolkit for Robots Comprehending Space
+// Copyright (C) 2011  Andrzej Pronobis
+//
+// This file is part of ROCS.
+//
+// ROCS is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or (at your option) any later version.
+//
+// ROCS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ROCS. If not, see <http://www.gnu.org/licenses/>.
+// ==================================================================
+
+/*!
+ * Main include file for the ROCS Core module.
+ *
+ * \author Andrzej Pronobis
+ * \file core.h
+ */
+
+#ifndef _ROCS_CORE_CORE_H_
+#define _ROCS_CORE_CORE_H_
+
+#include "rocs/core/Configuration.h"
+#include "rocs/core/FileInfo.h"
+
+#endif /** _ROCS_CORE_CORE_H_ */
