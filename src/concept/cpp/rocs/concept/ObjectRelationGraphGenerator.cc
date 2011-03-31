fe30a0e4fd8a93ad2987bf2c58ed756b283b810c
Andrzej Pronobis <a.pronobis@gmail.com>
2011-03-31 17:35:06 +0200

    Next bits implementing the object relation.

--- src/concept/cpp/rocs/concept/ObjectRelationGraphGenerator.cc
+++ src/concept/cpp/rocs/concept/ObjectRelationGraphGenerator.cc
@@ -37,6 +37,8 @@ namespace rocs {
 namespace concept {
 
 using namespace ml;
+using namespace std;
+
 
 ObjectRelationGraphGenerator::ObjectRelationGraphGenerator(ml::FactorGraph *fg,
 		ml::VariableClassSet *vcs, ml::FactorClassSet *fcs): _fg(fg), _vcs(vcs), _fcs(fcs),
@@ -55,15 +57,14 @@ ObjectRelationGraphGenerator::ObjectRelationGraphGenerator(ml::FactorGraph *fg,
 	}
 
 	// Creating relation variable classes
-	_onRelationVariableClass = &_vcs->addVariableClass(VVT_BOOL);
-	_ontRelationVariableClass = &_vcs->addVariableClass(VVT_BOOL);
-	_inRelationVariableClass = &_vcs->addVariableClass(VVT_BOOL);
+	_onRelationVariableClass = &_vcs->addVariableClass("on-relation", VVT_BOOL);
+	_ontRelationVariableClass = &_vcs->addVariableClass("ont-relation", VVT_BOOL);
+	_inRelationVariableClass = &_vcs->addVariableClass("in-relation", VVT_BOOL);
 
 	// Creating factor classes
 	_axiomFactorGraphGenerator = boost::shared_ptr<AxiomFactorClassGenerator>(
 			new AxiomFactorClassGenerator(_onRelationVariableClass,
 			_ontRelationVariableClass, _inRelationVariableClass, _fcs));
-	_axiomFactorGraphGenerator->generate();
 }
 
 
@@ -76,17 +77,114 @@ ObjectRelationGraphGenerator::~ObjectRelationGraphGenerator()
 }
 
 
+
+
 void ObjectRelationGraphGenerator::createObjectRelationVariables()
 {
+	// There is a relation for every pair of objects, unless an exception is added
+	for (size_t i=0; i<_objects.size(); ++i)
+	{
+		for (size_t j=0; j<_objects.size(); ++j)
+		{
+			string &o1 = _objects[i];
+			string &o2 = _objects[j];
 
+			// On
+			if (!onExceptionMatches(o1, o2))
+			{
+				Relation r;
+				r.object1Id = o1;
+				r.object2Id = o2;
+				r.variable = &_fg->addVariable(*_onRelationVariableClass);
+				_onRelations.push_back(r);
+			}
+
+			// Ont
+			if (!ontExceptionMatches(o1, o2))
+			{
+				Relation r;
+				r.object1Id = o1;
+				r.object2Id = o2;
+				r.variable = &_fg->addVariable(*_ontRelationVariableClass);
+				_ontRelations.push_back(r);
+			}
+
+			// In
+			if (!inExceptionMatches(o1, o2))
+			{
+				Relation r;
+				r.object1Id = o1;
+				r.object2Id = o2;
+				r.variable = &_fg->addVariable(*_inRelationVariableClass);
+				_inRelations.push_back(r);
+			}
+
+		}
+	}
 }
 
 
 void ObjectRelationGraphGenerator::createAxiomFactors()
 {
+	// We go through all the relation variables and create all proper factors.
 
 }
 
+
+void ObjectRelationGraphGenerator::createObservationFactors()
+{
+	// There is a factor for every observation, unless an exception is added
+	// On
+	for(size_t i=0; i<_onRelationObservations.size(); ++i)
+	{
+		RelationObservation &ro = _onRelationObservations[i];
+
+		// On
+		if (!onExceptionMatches(ro))
+		{
+			Relation *r = findOnRelation(ro.object1Id, ro.object2Id);
+			if (r)
+			{
+				double p[2] = {1-ro.value, ro.value};
+				cv::Mat potentials(2, 0, CV_64F, p);
+				_fg->addFactor(*r->variable, potentials);
+			}
+		}
+	}
+
+	// Ont
+	for(size_t i=0; i<_ontRelationObservations.size(); ++i)
+	{
+		RelationObservation &ro = _ontRelationObservations[i];
+		// Ont
+		if (!ontExceptionMatches(ro))
+		{
+			Relation *r = findOntRelation(ro.object1Id, ro.object2Id);
+			if (r)
+			{
+				double p[2] = {1-ro.value, ro.value};
+				cv::Mat potentials(2, 0, CV_64F, p);
+				_fg->addFactor(*r->variable, potentials);
+			}
+		}
+	}
+
+	for(size_t i=0; i<_inRelationObservations.size(); ++i)
+	{
+		RelationObservation &ro = _inRelationObservations[i];
+		// In
+		if (!inExceptionMatches(ro))
+		{
+			Relation *r = findInRelation(ro.object1Id, ro.object2Id);
+			if (r)
+			{
+				double p[2] = {1-ro.value, ro.value};
+				cv::Mat potentials(2, 0, CV_64F, p);
+				_fg->addFactor(*r->variable, potentials);
+			}
+		}
+	}
+}
 
 
 
--- src/concept/cpp/rocs/concept/ObjectRelationGraphGenerator.h
+++ src/concept/cpp/rocs/concept/ObjectRelationGraphGenerator.h
@@ -29,8 +29,10 @@
 #define _ROCS_CONCEPT_OBJECTRELATIONGRAPHGENERATOR_H_
 
 #include <rocs/concept/AxiomFactorClassGenerator.h>
+#include <rocs/ml/Variable.h>
 #include <boost/shared_ptr.hpp>
-
+#include <string>
+#include <vector>
 
 namespace rocs {
 
@@ -50,6 +52,50 @@ namespace concept {
  */
 class ObjectRelationGraphGenerator
 {
+	struct RelationObservation
+	{
+		RelationObservation(const std::string &object1Id_, const std::string &object2Id_, double value_):
+			object1Id(object1Id_), object2Id(object2Id_), value(value_)
+		{}
+
+		std::string object1Id;
+		std::string object2Id;
+		double value;
+	};
+
+	struct RelationException
+	{
+		RelationException(const std::string &object1Id_, const std::string &object2Id_):
+			object1Id(object1Id_), object2Id(object2Id_)
+		{}
+
+		std::string object1Id;
+		std::string object2Id;
+
+		bool matches(const std::string &object1Id_, const std::string &object2Id_)
+		{
+			return ( ((object1Id.empty()) || (object1Id == object1Id_))
+				    & ((object2Id.empty()) || (object2Id == object2Id_)) );
+		}
+
+		bool matches(const RelationObservation &ro)
+		{
+			return ( ((object1Id.empty()) || (object1Id == ro.object1Id))
+				    & ((object2Id.empty()) || (object2Id == ro.object2Id)) );
+		}
+	};
+
+
+public:
+
+	struct Relation
+	{
+		std::string object1Id;
+		std::string object2Id;
+
+		ml::Variable *variable;
+	};
+
 
 public:
 	ObjectRelationGraphGenerator(ml::FactorGraph *fg, ml::VariableClassSet *vcs = 0,
@@ -60,7 +106,58 @@ public:
 	void generate()
 	{
 		createObjectRelationVariables();
+//		_axiomFactorGraphGenerator->generate();  // TODO
 		createAxiomFactors();
+	}
+
+	void addObject(std::string id)
+	{
+		_objects.push_back(id);
+	}
+
+	void addInRelationObservation(const std::string &object1Id, const std::string &object2Id, double value)
+	{
+		_inRelationObservations.push_back(RelationObservation(object1Id, object2Id, value));
+	}
+
+	void addOntRelationObservation(const std::string &object1Id, const std::string &object2Id, double value)
+	{
+		_ontRelationObservations.push_back(RelationObservation(object1Id, object2Id, value));
+	}
+
+	void addOnRelationObservation(const std::string &object1Id, const std::string &object2Id, double value)
+	{
+		_onRelationObservations.push_back(RelationObservation(object1Id, object2Id, value));
+	}
+
+	/*!
+	 * Empty string matches every object.
+	 * @param object1Id
+	 * @param object2Id
+	 */
+	void addInRelationException(const std::string &object1Id, const std::string &object2Id)
+	{
+		_inRelationExceptions.push_back(RelationException(object1Id, object2Id));
+	}
+
+	/*!
+	 * Empty string matches every object.
+	 * @param object1Id
+	 * @param object2Id
+	 */
+	void addOntRelationObservation(const std::string &object1Id, const std::string &object2Id)
+	{
+		_ontRelationExceptions.push_back(RelationException(object1Id, object2Id));
+	}
+
+	/*!
+	 * Empty string matches every object.
+	 * @param object1Id
+	 * @param object2Id
+	 */
+	void addOnRelationException(const std::string &object1Id, const std::string &object2Id)
+	{
+		_onRelationExceptions.push_back(RelationException(object1Id, object2Id));
 	}
 
 
@@ -68,6 +165,99 @@ private:
 
 	void createObjectRelationVariables();
 	void createAxiomFactors();
+	void createObservationFactors();
+
+	bool onExceptionMatches(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t e=0; e<_onRelationExceptions.size(); ++e)
+		{
+			if (_onRelationExceptions[e].matches(object1Id, object2Id))
+				return true;
+		}
+		return false;
+	}
+
+	bool ontExceptionMatches(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t e=0; e<_ontRelationExceptions.size(); ++e)
+		{
+			if (_ontRelationExceptions[e].matches(object1Id, object2Id))
+				return true;
+		}
+		return false;
+	}
+
+	bool inExceptionMatches(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t e=0; e<_inRelationExceptions.size(); ++e)
+		{
+			if (_inRelationExceptions[e].matches(object1Id, object2Id))
+				return true;
+		}
+		return false;
+	}
+
+
+
+	bool onExceptionMatches(const RelationObservation &ro)
+	{
+		for (size_t e=0; e<_onRelationExceptions.size(); ++e)
+		{
+			if (_onRelationExceptions[e].matches(ro))
+				return true;
+		}
+		return false;
+	}
+
+	bool ontExceptionMatches(const RelationObservation &ro)
+	{
+		for (size_t e=0; e<_ontRelationExceptions.size(); ++e)
+		{
+			if (_ontRelationExceptions[e].matches(ro))
+				return true;
+		}
+		return false;
+	}
+
+	bool inExceptionMatches(const RelationObservation &ro)
+	{
+		for (size_t e=0; e<_inRelationExceptions.size(); ++e)
+		{
+			if (_inRelationExceptions[e].matches(ro))
+				return true;
+		}
+		return false;
+	}
+
+	Relation *findOnRelation(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t i=0; i<_onRelations.size(); ++i)
+		{
+			if ((_onRelations[i].object1Id == object1Id) && (_onRelations[i].object2Id == object2Id))
+				return &_onRelations[i];
+		}
+		return 0;
+	}
+
+	Relation *findInRelation(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t i=0; i<_inRelations.size(); ++i)
+		{
+			if ((_inRelations[i].object1Id == object1Id) && (_inRelations[i].object2Id == object2Id))
+				return &_inRelations[i];
+		}
+		return 0;
+	}
+
+	Relation *findOntRelation(const std::string &object1Id, const std::string &object2Id)
+	{
+		for (size_t i=0; i<_ontRelations.size(); ++i)
+		{
+			if ((_ontRelations[i].object1Id == object1Id) && (_ontRelations[i].object2Id == object2Id))
+				return &_ontRelations[i];
+		}
+		return 0;
+	}
 
 
 private:
@@ -83,6 +273,17 @@ private:
 	ml::VariableClass *_inRelationVariableClass;
 
 	boost::shared_ptr<AxiomFactorClassGenerator> _axiomFactorGraphGenerator;
+
+	std::vector<std::string> _objects;
+	std::vector<RelationObservation> _inRelationObservations;
+	std::vector<RelationObservation> _ontRelationObservations;
+	std::vector<RelationObservation> _onRelationObservations;
+	std::vector<RelationException> _inRelationExceptions;
+	std::vector<RelationException> _ontRelationExceptions;
+	std::vector<RelationException> _onRelationExceptions;
+	std::vector<Relation> _inRelations;
+	std::vector<Relation> _ontRelations;
+	std::vector<Relation> _onRelations;
 };
 
 
